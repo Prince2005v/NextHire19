48da1a6a73657b49d77a7d37db00ecf60bb77983
prince verma <8567prince@gmail.com>
2026-07-11 13:47:08 +0530

    your message

--- Desktop/dsa/stack.cpp
+++ Desktop/dsa/stack.cpp
@@ -1,43 +1,37 @@
 #include<iostream>
-#include <vector>
-#include<list>
-#include<stack>
+#include<vector>
 using namespace std;
 
-// class Stack{
-//   list<int> ll;
-
-// public:
-      
-//     void push(int val) {
-//       ll.push_front(val);
-//     }
-
-//      void pop() {
-//       ll.pop_front();
-
-//     }
-//      int top () {
-//       return ll.front(); 
-//     }
-
-//     bool empty(){
-//       return ll.size() == 0;
-//     }
-
-// };
-
-int main (){
-  stack<int> s;
-  s.push(10);
-  s.push(20);
-  s.push(30);
-
-  while(!s.empty()){
-    cout << s.top() << " ";
-    s.pop();
+class Stack {
+  vector<int> v;
+  public:
+  void push(int val){
+    v.push_back(val);
   }
-  cout << endl;
-  return 0;
-  
+  void pop(){
+    v.pop_back();
+  }
+  int top(){
+    return v[v.size()-1];
+  }
+  bool empty(){
+    return v.empty();
+  }
+};
+
+int main(){
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+
+    while(!s.empty()){
+      cout << s.top() << " ";
+      s.pop();
+    }
+    cout << endl;
+    return 0;
+    
 }
+
